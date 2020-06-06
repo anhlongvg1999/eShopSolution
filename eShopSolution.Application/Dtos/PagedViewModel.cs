91bb0f5dc63bae317b1d8ec0ed34f1baca86e353
= <=>
2020-06-06 16:33:27 +0700

    dựng cấu trúc application

---
+++ eShopSolution.Application/Dtos/PagedViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShopSolution.Application.Dtos
+{
+    public class PagedViewModel<T>
+    {
+        List<T> Items { set; get; }
+        public int TotalRecord { set; get; }//tổng số sản phẩm
+    }
+}
